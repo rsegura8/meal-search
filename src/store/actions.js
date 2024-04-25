944cfb835ac4b92999323c77751df1b97fac5a1d
rsegura8 <areyousure.rey@gmail.com>
2024-04-25 18:50:02 -0500

    Problem fixed! Meals By Ingerdient Done

--- src/store/actions.js
+++ src/store/actions.js
@@ -17,7 +17,7 @@ export function searchMealsByLetter({ commit }, letter) {
 }
 
 export function searchMealsByIngredient({ commit }, ingredient) {
-    api.get(`search.php?i=${ingredient}`)
+    api.get(`filter.php?i=${ingredient}`)
         .then(({ data }) => {
         commit('setMealsByIngredient', data.meals)
     })
